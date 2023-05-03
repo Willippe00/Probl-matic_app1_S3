7d4b3a4ad68929275ebdca164880bdcec7e61327
William Roberge <202059891@edu.clg.qc.ca>
2023-05-03 18:54:34 -0400

    Singleton add
    ajoute du modèle de conception singleton

--- MenuFact01/code/menufact/Menu.java
+++ MenuFact01/code/menufact/Menu.java
@@ -6,11 +6,13 @@ import menufact.plats.PlatAuMenu;
 import java.util.ArrayList;
 
 public class Menu {
+
+    private static Menu instance;
     private String description;
     private int courant;
     private ArrayList<PlatAuMenu> plat = new ArrayList<PlatAuMenu>();
 
-    public Menu(String description) {
+    private Menu(String description) {
         this.description = description;
     }
 
@@ -53,5 +55,13 @@ public class Menu {
                 ", courant=" + courant +
                 ", plat=" + "\n" + plat +
                 '}';
+    }
+
+    public static synchronized Menu getInstance(String description){
+
+        if (instance == null)
+            instance = new Menu(description);
+
+        return instance;
     }
 }
