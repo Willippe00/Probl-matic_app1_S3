1dd4b3167bc66d1a96f32d6ae68c9fc126d0511c
William Roberge <202059891@edu.clg.qc.ca>
2023-05-09 10:17:44 -0400

    test du cheff
    oui

---
+++ MenuFact01/code/tests/ChefTest.java
@@ -0,0 +1,45 @@
+package tests;
+
+import menufact.Chef.Chef;
+import menufact.Chef.Commander;
+import menufact.plats.PlatAuMenu;
+import menufact.plats.PlatChoisi;
+import menufact.plats.PlatSante;
+import menufact.plats.exeptions.ServiceException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+//package menufact.Chef;
+
+        import menufact.plats.PlatAuMenu;
+        import menufact.plats.PlatChoisi;
+        import menufact.plats.PlatSante;
+        import menufact.plats.exeptions.ServiceException;
+        import org.junit.jupiter.api.Test;
+
+
+
+        import static org.junit.jupiter.api.Assertions.*;
+
+class ChefTest {
+    @Test
+    void Test_du_Chef()
+    {
+
+        Chef Gaston;
+        Gaston = Chef.getIntance();
+        assertNotNull(Gaston );
+    }
+
+    @Test
+    void Test_notification() throws ServiceException {
+
+        PlatAuMenu PlatSante = new PlatSante();
+        Commander Gaston2 = new Commander(new PlatChoisi(PlatSante, 6));
+
+        assert (Gaston2.middleware!=null);
+
+    }
+}
